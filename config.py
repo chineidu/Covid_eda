c043058f2f0c515b56b4199f67f92556acbf1361
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2021-06-12 20:21:44 +0100

    initialized repo

---
+++ config.py
@@ -0,0 +1,58 @@
+import os
+import mysql.connector as sql
+
+# configuration
+config = {
+    'host': 'localhost',
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PSWD')
+}
+# create connection using configuration
+conn = sql.connect(**config)
+# create cursor object
+my_cursor = conn.cursor()
+
+
+# Helper Functions
+def create_db(DB_NAME):
+    """Create a MySQL DB"""
+    sql_query = f"CREATE SCHEMA IF NOT EXISTS {DB_NAME}"
+    my_cursor.execute(sql_query)
+    print(f"DB: {DB_NAME} was created \n")    
+
+def create_table(table_name, sql_query):
+    """Add a table to the DB"""
+    # select DB
+    my_cursor.execute(f""" USE {DB_NAME} """)
+    # execute the query
+    my_cursor.execute(sql_query)
+    print(f"Table: {table_name} was created \n")    
+
+def show_tables():
+    # check for the tables
+    my_cursor.execute('SHOW TABLES')
+    # display tables
+    for table in my_cursor:
+        print(table)
+        print()    # new line
+        
+def insert_into_table(table_name, sql_query, values):
+    """Insert values into a table"""
+    # add all the rows to the db
+    my_cursor.executemany(sql_query, values)
+    # commit the changes
+    conn.commit()
+    print(f"Values inserted into {table_name}")
+     
+def select_records(sql_query):
+    """Select records from a table"""
+    my_cursor.execute(sql_query)
+    # display records
+    for x in my_cursor:
+        print(f"{x} \n")
+        
+def close_conn():
+    """Close the connection to the DB"""
+    conn.close()
+    print(f"Connection to {DB_NAME} has been closed!")
+
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Covid19 EDA Project
+
+## Data
+
+The data was obtained from [Our World in Data](https://ourworldindata.org/covid-deaths). It contains Covid 19 data up until 7th June, 2021.
+
+## Data Cleaning
+
+The data was cleaned using Python.
+
+* Null values and missing values were properly handled.
+* The records with missing continent were dropped.
+* Null values in numerical columns were replaced with 0.
+* Null values in categorical columns with were replace 'NULL'.
+* The data was converted to the appropriate data types.
+
+## ETL
+
+Custom Python functions were written which were used to:
+
+* create a MySQL connection using mysql-connector.
+* create MySQL database.
+* create tables.
+* populate the tables.
+* select records from the tables.
+* close the connection.
+
+## EDA
+
+* EDA was carried out in Jupyter notebook and MySQL.
+* Several queries were written in MySQL.
+
+## Dashboard
+
+A dashboard was built using Tableau Public which can be found [here](https://public.tableau.com/app/profile/chinedu.ezeofor/viz/Covid19_dashboard_16235235313490/Dashboard1)
