91832e4066284beb051417f9140b75d2c1576de6
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2021-06-13 22:53:37 +0100

    modified config.py

--- config.py
+++ config.py
@@ -7,52 +7,53 @@ config = {
     'user': os.environ.get('DB_USER'),
     'password': os.environ.get('DB_PSWD')
 }
-# create connection using configuration
-conn = sql.connect(**config)
-# create cursor object
-my_cursor = conn.cursor()
-DB_NAME = 'covid_eda_db'
 
-# Helper Functions
-def create_db(DB_NAME):
-    """Create a MySQL DB"""
-    sql_query = f"CREATE SCHEMA IF NOT EXISTS {DB_NAME}"
-    my_cursor.execute(sql_query)
-    print(f"DB: {DB_NAME} was created \n")    
-
-def create_table(table_name, sql_query):
-    """Add a table to the DB"""
-    # select DB
-    my_cursor.execute(f""" USE {DB_NAME} """)
-    # execute the query
-    my_cursor.execute(sql_query)
-    print(f"Table: {table_name} was created \n")    
-
-def show_tables():
-    # check for the tables
-    my_cursor.execute('SHOW TABLES')
-    # display tables
-    for table in my_cursor:
-        print(table)
-        print()    # new line
+class MySqlDb:
+    def __init__(self, config: dict):
+        """Initialize the values"""
+        # create connection using config
+        self.conn = sql.connect(**config)
+        # create cursor object
+        self.my_cursor = self.conn.cursor()
         
-def insert_into_table(table_name, sql_query, values):
-    """Insert values into a table"""
-    # add all the rows to the db
-    my_cursor.executemany(sql_query, values)
-    # commit the changes
-    conn.commit()
-    print(f"Values inserted into {table_name}")
-     
-def select_records(sql_query):
-    """Select records from a table"""
-    my_cursor.execute(sql_query)
-    # display records
-    for x in my_cursor:
-        print(f"{x} \n")
-        
-def close_conn():
-    """Close the connection to the DB"""
-    conn.close()
-    print(f"Connection to {DB_NAME} has been closed!")
+    def create_db(self, DB_NAME):
+        """Create a MySQL DB"""
+        sql_query = f"CREATE SCHEMA IF NOT EXISTS {DB_NAME}"
+        self.my_cursor.execute(sql_query)
+        print(f"DB: {DB_NAME} was created \n")    
 
+    def create_table(self, table_name, sql_query):
+        """Add a table to the DB"""
+        # select DB
+        self.my_cursor.execute(f""" USE {DB_NAME} """)
+        # execute the query
+        self.my_cursor.execute(sql_query)
+        print(f"Table: {table_name} was created \n")    
+
+    def show_tables(self):
+        # show the tables
+        self.my_cursor.execute('SHOW TABLES')
+        # display tables
+        print('Table names:')
+        for table in self.my_cursor:
+            print(f"{table} \n")
+
+    def insert_into_table(self, table_name, sql_query, values):
+        """Insert values into a table"""
+        # add all the rows to the db
+        self.my_cursor.executemany(sql_query, values)
+        # commit the changes
+        self.conn.commit()
+        print(f"Values inserted into {table_name} \n")
+
+    def select_records(self, sql_query):
+        """Select records from a table"""
+        self.my_cursor.execute(sql_query)
+        # display records
+        for x in self.my_cursor:
+            print(f"{x} \n")
+
+    def close_conn(self):
+        """Close the connection to the DB"""
+        self.conn.close()
+        print(f"Connection to {DB_NAME} has been closed!") 
